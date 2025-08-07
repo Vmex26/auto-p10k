2e7d31e37bc58c044a6a1ca2553c8957962ab0db
Vmex26 <Hanxdlol2602@gmail.com>
2025-08-07 00:38:01 -0500

    funcion confirm termux

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-#Aqui ira el codigo del proyecto
+#!/usr/bin/env bash
+
+#Confirma que estamos en termux
+
+termux_confirm() {
+  if [ -n "$TERMUX_VERSION" ]; then
+   echo "zi termux"
+    #Aqui llamaremos a una funcion para ejecutar
+    #el codigo 
+  else
+    echo "[!] Este script solo funciona en termux" 
+fi
+}
+
+termux_confirm
