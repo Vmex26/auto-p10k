1fd1c31031ade94127c7c1da065bbd00c0db0618
Vmex26 <Hanxdlol2602@gmail.com>
2025-08-07 11:00:20 -0500

    Confirmacion de root

--- install.sh
+++ install.sh
@@ -4,12 +4,36 @@
 
 termux_confirm() {
   if [ -n "$TERMUX_VERSION" ]; then
-   echo "zi termux"
-    #Aqui llamaremos a una funcion para ejecutar
-    #el codigo 
+    echo "zi termux"
+    check_root
   else
     echo "[!] Este script solo funciona en termux" 
-fi
+    echo "[?] Desea continuar? (yes/no)"
+    read confirm
+    if [ $confirm == "yes" ]; then
+      
+      echo "oka"
+      check_root
+    else 
+      echo "por favor escriba 'yes' para continuar "
+    fi
+  fi
 }
+
+check_dependency() {
+  #Aqui vamos a confirmar que esten las dependencias
+  echo "Bien"
+}
+
+check_root() {
+  user=$(whoami)
+  if [ $user == "root" ]; then
+    echo "[!] No ejecute el script como root"
+    echo "[!] Saliendo..."
+    exit 2
+  else
+    check_dependency
+  fi
+ }
 
 termux_confirm
