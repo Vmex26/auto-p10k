217232abd73044110d0355f918bfc5e7655caf8e
Vmex26 <Hanxdlol2602@gmail.com>
2025-08-07 16:42:37 -0500

    Verificar dependecias

--- install.sh
+++ install.sh
@@ -15,20 +15,37 @@ termux_confirm() {
       echo "oka"
       check_root
     else 
-      echo "por favor escriba 'yes' para continuar "
+      echo "por favor escriba 'yes' para continuar"
     fi
   fi
 }
 
 check_dependency() {
   #Aqui vamos a confirmar que esten las dependencias
-  echo "Bien"
+  #Instalaremos las dependencias nesesarias
+  DEPENDENCIAS=(git curl wget zsh)
+
+  for cmd in "${DEPENDENCIAS[@]}"; do
+    if ! command -v $cmd &> /dev/null; then
+        echo "[!] $cmd no está instalado, Instalando.."
+        pkg install -y $cmd
+    else
+        echo "[¡] Dependencia $cmd satisfecha"
+    fi
+  done
+  install_oh-my-zsh
+}
+
+install_oh-my-zsh() {
+  pass
+  #Aqui incluire el codigo que instale oh my zsh
 }
 
 check_root() {
+  #Chekeamos que no se ejecute en root o con sudo
   user=$(whoami)
   if [ $user == "root" ]; then
-    echo "[!] No ejecute el script como root"
+    echo "[!] No ejecute el script como root/sudo"
     echo "[!] Saliendo..."
     exit 2
   else
